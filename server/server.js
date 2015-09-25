819865eb86429d430b630ad80518249a8654b7c6
Liuminosity <liu.stanford@gmail.com>
2015-09-24 19:57:50 -0700

    [feat] (tests): connect tests to server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
 //this is placeholder file for server.js
+
+var express = require('express');
+var app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello, world!");
+}) 
+
+app.post("/", function(req, res) {
+  res.send("You posted!");
+})
+
+var server = app.listen((process.env.PORT || 3000), function () {
+  var host = server.address().address;
+  var port = server.address().port;
+});
+
+
+if (__filename === process.argv[1]) {
+  app.listen(6789);
+}
+
+module.exports = app;
---
+++ tests/serverSpec.js
@@ -0,0 +1,19 @@
+//this is placeholder file for serverTests.js
+var request = require('request');
+
+//Specs for server
+describe('Server', function() {
+  it('should work', function() {
+    expect(true).toBe(true);
+  })
+  it("should respond with hello world", function(done) {
+    request("http://localhost:6789", function(error, response, body){
+      expect(body).toEqual("Hello, world!");
+      done();
+    });
+  });
+})
+
+
+
+
