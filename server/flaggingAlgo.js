1a93f5e376d2e7aeeb1e5fc40967761ed9ba1c8e
gtdeng <gtdeng@gmail.com>
2015-09-30 15:23:45 -0700

    [feat] (algo): build createContext fx

--- server/flaggingAlgo.js
+++ server/flaggingAlgo.js
@@ -4,12 +4,6 @@ var database = require('./database.js');
 //fx to pull all email from the db and then store them temporarily
 var uncheckedEmails = database.getUncheckedEmails;
 
-//fx to remove all the new lines in the emailList table and store it in antoher columns
-
-
-//fx to insert CONTEXT into the database,
-//context = sentence before, sentence, sentence after
-
 //fx to update the emailList table
 var markChecked = function(emailID) {
   var checkString = 'UPDATE emailList SET checked=1 WHERE id=' + emailID;
@@ -25,7 +19,16 @@ var markFlagged = function(emailID) {
 //fx to createContext
 var createContext = function(email, flaggedWord) {
   var text = email.text;
+  //find the index of the flaggedWord
+  var index = text.search(flaggedWord);
+  var numOfChars = 200;
+  var start = index - numOfChars;
+  var end = numOfChars + index;
+  //get a substring of 200chars before and 200 after
+  var context = text.substring(start, end);
 
+  //return the string.
+  return context;
 };
 
 //fx to insert into the flagged content context table
@@ -33,28 +36,38 @@ var insertIntoContextTable = function(emailID, flaggedWord, context) {
 
 };
 
+//fx to push tag to the database
+var writeTag = function(word, emailID) {};
+
+//fx to remove all the new lines in the emailList table and store it in antoher columns
+
+
+//fx to insert CONTEXT into the database,
+//context = sentence before, sentence, sentence after
+
 //fx to check emails for bad words
 var filterEmails = function(emailArray){
-  // //loop thru the responseArray
-  // for (var i=0; i<emailArray.length; i++){
-  // // change checked value to 1 in the emailList table;
-  //   markChecked(emailArray[i].id);
-  // //loop thru the badWordsArray
-  // //ToDo: update badWords
-  //   for (var j=0; j<badWords.length; j++) {
-  //     var subString = new RegExp(badWords[j]);
-  //     //if object's TEXT value contains that 'bad word'
-  //     if (subString.test(emailArray[i].text)) {
-  //       // change flag value to 1 in the emailList table;
-  //       markFlagged(emailArray[j].id)
-  //       //create context
-  //       var context = createContext(emailArray[i], badWords[j]);
-  //       //push the 'flaggedKeyWord', the 'id', the 'context' to flaggedContextList table's flaggedKeyWord column
-  //       insertIntoContextTable(emailArray[i].id, badWords[j], context);
-  //     }
-  //   }
+  //loop thru the responseArray
+  for (var i=0; i<emailArray.length; i++){
+    var theEmail = emailArray[i];
+    // change checked value to 1 in the emailList table;
+    markChecked(theEmail.id);
+    //loop thru the badWordsArray
+    //ToDo: update badWords
+    for (var j=0; j<badWords.length; j++) {
+      var subString = new RegExp(badWords[j]);
+      //if object's TEXT value contains that 'bad word'
+      if (subString.test(theEmail.text)) {
+        // change flag value to 1 in the emailList table;
+        markFlagged(emailArray[j].id)
+        //create context
+        var context = createContext(theEmail, badWords[j]);
+        //push the 'flaggedKeyWord', the 'id', the 'context' to flaggedContextList table's flaggedKeyWord column
+        insertIntoContextTable(theEmail.id, badWords[j], context);
+      }
+    }
 
-  // }
+  }
 };
 
 filterEmails(uncheckedEmails());
@@ -68,8 +81,6 @@ var checkEmails = function(emailText, emailID) {
   }
 };
 
-//fx to push tag to the database
-var writeTag = function(word, emailID) {};
 
 //temp small list of bad words
 var badWords = ['damn', 'shit', 'crap', 'butt'];
