3e0832e4b77439f893e840c3e4688b79442bc098
gtdeng <gtdeng@gmail.com>
2015-10-05 18:28:03 -0700

    [feat] (algo): algo now able to search from keywordTable

--- server/flaggingAlgo.js
+++ server/flaggingAlgo.js
@@ -2,14 +2,15 @@
 var database = require('./database.js');
 var cron = require('cron');
 
-//temp small list of bad words
-// var badWords = ['damn', 'shit', 'crap', 'butt'];
-
 //import fx
 var getUncheckedEmails = database.getUncheckedEmails;
 var markChecked = database.markChecked;
 var markFlagged = database.markFlagged;
 var insertIntoContextTable = database.insertIntoContextTable;
+var getFlaggedWords = database.getFlaggedWords;
+
+//temp small list of bad words
+// var badWords = ['damn', 'shit', 'crap', 'butt'];
 
 //fx to createContext. context = a substring of 200chars before and 200 after the flaggedKeyWord
 var createContext = function(email, flaggedKeyWord) {
@@ -23,7 +24,7 @@ var createContext = function(email, flaggedKeyWord) {
   return context;
 };
 
-//fx to check emails for bad words and then store it into the contextTable
+//fx to check emails for keywords and then store it into the contextTable
 var filterEmail = exports.filterEmail = function(emailArray) {
   console.log('filterEmail fx ran/////');
   console.log('filterEmailz emailArray argument is ///////.....', emailArray);
@@ -31,32 +32,29 @@ var filterEmail = exports.filterEmail = function(emailArray) {
   //loop thru the responseArray
   for (var i = 0; i < emailArray.length; i++) {
     var email = emailArray[i];
-    console.log('heeeeeeeee', typeof markFlagged)
 
     // change checked value to 1 in the emailTable
     markChecked(email.id);
 
-    // loop thru the badWordsArray
-    // TODO: update badWords
-    var flaggedWords = [];
-    
+    // loop thru the flaggedWords array from keywordTable
+    getFlaggedWords(function(flaggedWords) {
+      for (var j = 0; j < flaggedWords.length; j++) {
+        var keyword = flaggedWords[j];
+        var subString = new RegExp(keyword.keyword, 'ig');
 
-    for (var j = 0; j < flaggedWords.length; j++) {
-      // console.log('this is i', i, ' and this is j', j);
-      var subString = new RegExp(flaggedWords[j], 'ig');
+        //if object's TEXT value contains that 'bad word'
+        if (subString.test(email.text)) {
+          // change flag value to 1 in the emailTable ;
+          markFlagged(email.id);
 
-      //if object's TEXT value contains that 'bad word'
-      if (subString.test(email.text)) {
-        // change flag value to 1 in the emailTable ;
-        markFlagged(email.id);
+          // //create context
+          var context = createContext(email, keyword.keyword);
 
-        // //create context
-        var context = createContext(email, flaggedWords[j]);
-
-        // //insert the 'flaggedKeyWord', the 'id', the 'context' into contextTable
-        insertIntoContextTable(email.id, flaggedWords[j], context);
+          // //insert the 'flaggedKeyWord', the 'id', the 'context' into contextTable
+          insertIntoContextTable(keyword.userID, keyword.filterID, email.id, keyword.keyword, context);
+        }
       }
-    }
+    });
   }
 
   console.log('filterEmail fx is done running/////');
