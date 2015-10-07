60b5c716f0bd0c18b4460ac71566553076f2a2c7
Liuminosity <liu.stanford@gmail.com>
2015-10-06 19:42:16 -0700

    [feat] (client): add more code for client auth--still not functional

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import emails from './emails';
+import users from './users';
 
 export default combineReducers({
   counter,
   emails,
+  users,
 });
