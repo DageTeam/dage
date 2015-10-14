6bd5a0ac9e23f132d9629364332362158c77c166
Tito Esteves <titoesteves@live.com>
2015-10-13 20:40:21 -0700

    [feat](client): reverted to griddle until other manageuser component is complete

--- src/components/ManageUsers.jsx
+++ src/components/ManageUsers.jsx
@@ -1,11 +1,59 @@
 var React = require('react');
 var Griddle = require('griddle-react');
+var ReactBsTable = require("react-bootstrap-table");
+var BootstrapTable = ReactBsTable.BootstrapTable;
+var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
 
 var ManageUsers = React.createClass({
+  products: [
+    {
+        id: 1,
+        name: "Product1",
+        price: 120
+    },{
+        id: 2,
+        name: "Product2",
+        price: 80
+    },{
+        id: 3,
+        name: "Product3",
+        price: 207
+    },{
+        id: 4,
+        name: "Product4",
+        price: 100
+    },{
+        id: 5,
+        name: "Product5",
+        price: 150
+    },{
+        id: 6,
+        name: "Product1",
+        price: 160
+    }
+  ],
+  selectRowProp: {
+    mode: "checkbox",  //checkbox for multi select, radio for single select.
+    clickToSelect: true,   //click row will trigger a selection on that row.
+    bgColor: "rgb(238, 193, 213)"   //selected row background color
+  },
+  addUser: function(){
+    var modal = document.getElementById('myModal');
+    $('#myModal').modal();
+  },
   render: function() {
     return (
       <div className='container'>
-      
+        <Griddle results={this.props.userArray} showSettings={true} className={this.styles.table}
+           useFixedLayout={false} showFilter={true} showSettings={true} useGriddleStyles={false} 
+           columns={['username',
+                     'title',
+                     'email',
+                     'department',
+                     'managerID',
+                     'active'
+                     ]} 
+           noDataMessage={'No data could be found.'}/>
       </div>
       );
   },
