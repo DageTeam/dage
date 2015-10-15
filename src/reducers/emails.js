b929755e69b5d3f0149ea6121528b0f5430621ca
Liuminosity <liu.stanford@gmail.com>
2015-10-15 11:54:12 -0700

    [feat] (client): add new allEmails tab and view

--- src/reducers/emails.js
+++ src/reducers/emails.js
@@ -4,14 +4,27 @@ import {
   EMAIL_ARRAY_REQUEST,
   EMAIL_ARRAY_FETCH_SUCCESS,
   EMAIL_ARRAY_FETCH_ERROR,
+
+  ALL_EMAIL_FETCH_REQUEST,
+  ALL_EMAIL_FETCH_SUCCESS,
+  ALL_EMAIL_FETCH_ERROR,
+
+  CLEAR_ALL_EMAILS_CACHE,
+
   EMAIL_SHOW_ONE_FLAG,
   EMAIL_SHOW_ALL_FLAGS,
   EMAIL_SHOW_COMPLETE,
+
+  ALL_EMAILS_SHOW_ONE_FLAG,
+  ALL_EMAILS_SHOW_ALL_FLAGS,
+  ALL_EMAILS_SHOW_COMPLETE,
+
   REQUESTING_EMAIL_UNFLAG,
   EMAIL_UNFLAG_ERROR,
   EMAIL_UNFLAG_SUCCESS,
   EMAIL_MARK_READ_REQUEST,
   EMAIL_MARK_READ_SUCCESS,
+
 } from 'constants/emails';
 
 // const emailsArray = [
@@ -39,9 +52,11 @@ import {
 
 const emails = {
   isFetchingEmail: true,
+  isFetchingAllEmails: false,
   lastUpdated: 0,
   fetchingEmailError: '',
   emailsArray: [],
+  allEmailsArray: [],
 };
 
 const initialState = emails;
@@ -71,6 +86,36 @@ export default createReducer(initialState, {
     return state;
   },
 
+  [ALL_EMAIL_FETCH_REQUEST]: (state, payload) => {
+    return {
+      ...state,
+      isFetchingAllEmails: true,
+    }
+  },
+
+  [ALL_EMAIL_FETCH_SUCCESS]: (state, payload) => {
+    console.log('inside all email fetch success, this is payload', payload);
+    return {
+      ...state,
+      isFetchingAllEmails: false,
+      allEmailsArray: payload.allEmailsArray,
+    }
+  },
+
+  [ALL_EMAIL_FETCH_ERROR]: (state, payload) => {
+    return {
+      ...state,
+      isFetchingAllEmails: false,
+    }
+  },
+
+  [CLEAR_ALL_EMAILS_CACHE]: (state, payload) => {
+    return {
+      ...state,
+      allEmailsArray: [],
+    }
+  },
+
   [EMAIL_SHOW_ONE_FLAG]: (state, payload) => {
     let newState = Object.assign({}, state);
     let { emailId } = payload;
@@ -97,6 +142,40 @@ export default createReducer(initialState, {
     let newState = Object.assign({}, state);
     let { emailId } = payload;
     newState.emailsArray.forEach((email, index, list) => {
+      if (emailId === email.id) {
+        list[index].focusLevel = 'complete';
+      }
+    });
+    return newState;
+  },
+
+  //this section is for all the show/hide commands for allEmails tab
+  [ALL_EMAILS_SHOW_ONE_FLAG]: (state, payload) => {
+    let newState = Object.assign({}, state);
+    let { emailId } = payload;
+    newState.allEmailsArray.forEach((email, index, list) => {
+      if (emailId === email.id) {
+        list[index].focusLevel = 'one';
+      }
+    });
+    return newState;
+  },
+
+  [ALL_EMAILS_SHOW_ALL_FLAGS]: (state, payload) => {
+    let newState = Object.assign({}, state);
+    let { emailId } = payload;
+    newState.allEmailsArray.forEach((email, index, list) => {
+      if (emailId === email.id) {
+        list[index].focusLevel = 'allFlags';
+      }
+    });
+    return newState;
+  },
+
+  [ALL_EMAILS_SHOW_COMPLETE]: (state, payload) => {
+    let newState = Object.assign({}, state);
+    let { emailId } = payload;
+    newState.allEmailsArray.forEach((email, index, list) => {
       if (emailId === email.id) {
         list[index].focusLevel = 'complete';
       }
