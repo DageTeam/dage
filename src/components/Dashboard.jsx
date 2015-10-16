9ef7dc5b2721654f3834ac40e3e398cc6a9c8348
Max Li <maximilianm.li@gmail.com>
2015-10-15 18:03:47 -0700

    [feat](dashboard): added a most flagged user table

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,10 @@ var React = require('react');
 var ReactDOM = require('react-dom')
 var BarChart = require("react-chartjs").Bar;
 var DoughnutChart = require('react-chartjs').Doughnut
+var FixedDataTable = require('fixed-data-table'); 
+
+var Table = FixedDataTable.Table;
+var Column = FixedDataTable.Column;
 
 
 var Dashboard = React.createClass({
@@ -45,25 +49,250 @@ var Dashboard = React.createClass({
         barStrokeWidth : 2
         };
 
-    var doughnutData =[
+    var rows = [
+      ['sara.shackleton@enron.com',525],
+      ['richard.b.sanders@enron.com' ,494],
+      ['tana.jones@enron.com',  225],
+      ['steven.j.kean@enron.com', 225],
+      ['susan.mara@enron.com',  135],
+      ['susan.scott@enron.com', 118],
+      ['richard.shapiro@enron.com', 98],      
+      ['webmaster@cera.com',  89],
+      ['taylor@enron.com',  73],
+      ['showard@agsk.com' , 57],
+      ['showard@milbank.com', 52],
+      ['sarah.novosel@enron.com', 47],
+      ['sally.beck@enron.com' , 47],
+      ['technology.enron@enron.com' , 45],
+      ['rosalee.fleming@enron.com' ,42],
+      ['rod.hayslett@enron.com'  ,41],    
+      ['raislerk@sullcrom.com' ,38],
+      ['sheila.glover@enron.com' ,37],
+      ['tori.kuykendall@enron.com' ,31],
+      ['scott.neal@enron.com' , 31],
+      ['smarra@isda.org', 30],
+      ['shelley.corman@enron.com' , 30],
+      ['ray.alvarez@enron.com' ,30],
+      ['rcarroll@bracepatt.com' , 29],
+      ['stephanie.sever@enron.com' ,27],
+      ['rick.buy@enron.com ', 27],
+      ['venturewire@venturewire.com' ,26],
+      ['sarah.palmer@enron.com', 26],
+      ['seabron.adamson@frontier-economics.com',  25],
+      ['trnews@tr.com', 23],
+      ['t..hodge@enron.com',  23],
+      ['schwabalerts.marketupdates@schwab.com', 23],
+      ['tim.belden@enron.com ', 22],
+      ['sladana-anna.kulic@enron.com',  22],
+      ['w..cantrell@enron.com ',21],
+      ['travis.mccullough@enron.com', 21],
+      ['steve.c.hall@enron.com'  ,21],
+      ['stacey.w.white@enron.com' , 21],
+      ['robin.rodrigue@enron.com'  ,21],
+      ['stephanie.panus@enron.com' ,20],
+      ['stanley.horton@enron.com' , 20],
+      ['sheri.a.righi@accenture.com', 20],
+      ['vkaminski@aol.com' ,19],
+      ['spatti@ensr.com' ,19],
+      ['ruth.concannon@enron.com',  19],
+      ['rob.walls@enron.com' ,19],
+      ['wsmith@wordsmith.org',  18],
+      ['teresa.bushman@enron.com',  18],
+      ['stephanie.miller@enron.com',  18],
+      ['sharonda.stephens@enron.com', 17],
+      ['rob.gay@enron.com' ,17]
+    ];
+
+    var rowGetter = function (rowIndex) {
+      return rows[rowIndex];
+    };
+
+    var doughnutDataEthics =[
           {
-              value: 300,
-              color:"#F7464A",
-              highlight: "#FF5A5E",
-              label: "Red"
+              value: 64,
+              color:"#4169E1",
+              highlight: "#4169E1",
+              label: "unethical"
           },
           {
-              value: 50,
+              value: 14683,
+              color: "#7A67EE",
+              highlight: "#7A67EE",
+              label: "confidential"
+          },
+          {
+              value: 11300,
+              color: "#9400D3",
+              highlight: "#9400D3",
+              label: "delete"
+          },          
+          {
+              value: 2324,
+              color: "#8968CD",
+              highlight: "#8968CD",
+              label: "secret"
+          },
+          {
+              value: 2077,
+              color: "#7A378B",
+              highlight: "#7A378B",
+              label: "eliminate"
+          },
+          {
+              value: 1453,
+              color: "#68228B",
+              highlight: "#68228B",
+              label: "hide"
+          },
+          {
+              value: 1453,
+              color: "#0000EE",
+              highlight: "#0000EE",
+              label: "hide"
+          },
+          {
+              value: 1195,
+              color: "#7B68EE",
+              highlight: "#7B68EE",
+              label: "sensitive"
+          },
+          {
+              value: 667,
               color: "#46BFBD",
               highlight: "#5AD3D1",
-              label: "Green"
+              label: "illegal"
           },
           {
-              value: 100,
+              value: 0,
+              color:"#F7464A",
+              highlight: "#FF5A5E",
+              label: "noncompliant"
+          },
+          {
+              value: 636,
+              color: "#7D26CD",
+              highlight: "#7D26CD",
+              label: "unlawful"
+          },
+          {
+              value: 659,
               color: "#FDB45C",
               highlight: "#FFC870",
-              label: "Yellow"
+              label: "manipulation"
+          },
+          {
+              value: 266,
+              color: "#8A2BE2",
+              highlight: "#8A2BE2",
+              label: "scandal"
+          },          
+          {
+              value: 201,
+              color: "#46BFBD",
+              highlight: "#5AD3D1",
+              label: "get rid of"
+          },
+          {
+              value: 166,
+              color: "#F8F8FF",
+              highlight: "#F8F8FF",
+              label: "shred"
           }
+
+      ];
+
+      var doughnutDataCompetitors =[
+          {
+              value: 64,
+              color:"#097054",
+              highlight: "#FF5A5E",
+              label: "Dynergy"
+          },
+          {
+              value: 1453,
+              color: "#FFDE00",
+              highlight: "#FFDE00",
+              label: "Reliant Energy"
+          },
+          {
+              value: 667,
+              color: "#FF9900",
+              highlight: "#FF9900",
+              label: "Williams"
+          },
+          {
+              value: 166,
+              color: "#6599FF",
+              highlight: "#6599FF",
+              label: "El Paso"
+          }
+          // {
+          //     value: 1195,
+          //     color: "#46BFBD",
+          //     highlight: "#5AD3D1",
+          //     label: "sensitive"
+          // },
+          // {
+          //     value: 14683,
+          //     color: "#B0171F",
+          //     highlight: "#5AD3D1",
+          //     label: "confidential"
+          // },          
+          // {
+          //     value: 2324,
+          //     color: "#FFAEB9",
+          //     highlight: "#FFAEB9",
+          //     label: "secret"
+          // },
+          // {
+          //     value: 0,
+          //     color:"#8B5F65",
+          //     highlight: "#8B5F65",
+          //     label: "noncompliant"
+          // },
+          // {
+          //     value: 636,
+          //     color: "#FFF0F5",
+          //     highlight: "#FFF0F5",
+          //     label: "unlawful"
+          // },
+          // {
+          //     value: 659,
+          //     color: "#EEE0E5",
+          //     highlight: "#EEE0E5",
+          //     label: "manipulation"
+          // },
+          // {
+          //     value: 266,
+          //     color: "#CDC1C5",
+          //     highlight: "#CDC1C5",
+          //     label: "scandal"
+          // },
+          // {
+          //     value: 1453,
+          //     color: "#FF3E96",
+          //     highlight: "#FF3E96",
+          //     label: "hide"
+          // },
+          // {
+          //     value: 11300,
+          //     color: "#872657",
+          //     highlight: "#872657",
+          //     label: "delete"
+          // },          
+          // {
+          //     value: 201,
+          //     color: "#8B7B8B",
+          //     highlight: "#8B7B8B",
+          //     label: "get rid of"
+          // },
+          // {
+          //     value: 2077,
+          //     color: "#D15FEE",
+          //     highlight: "#D15FEE",
+          //     label: "eliminate"
+          // },
+
       ];
 
     var doughnutOptions = {
@@ -94,23 +323,46 @@ var Dashboard = React.createClass({
 
     return(
       <div>
-        <div style={{'marginTop':'8%','marginLeft':'15%', 'width':'75%'}}>
+          <div style={{'marginTop':'8%','marginLeft':'5%', 'width':'65%', 'float':'left'}}>
           <h2>You Have <a onClick = {this.navigateAlert} className='alertNum'>{emailNum}</a> New Alerts Right Now</h2>
-          <h3 style={{'marginBottom':'2%','textAlign':'left'}}>Flagged Emails by month</h3>
-          <BarChart data={barData} options={barOptions} width={'1000px'} height={'200px'} redraw={true} style={{'marginLeft':'2%'}}/>
           <div style={{'marginTop': '3%', 'marginLeft':'7%', float:'left'}}>
-          <h3>Flags by Filters (this week)</h3>
-          <DoughnutChart data={doughnutData} options ={doughnutOptions} width={'150px'} height = {'150px'} redraw = {true} style={{'marginLeft':'20px'}}/>
+            <h3 style={{'marginBottom':'2%','textAlign':'left'}}>Flagged Emails by month</h3>
+            <BarChart data={barData} options={barOptions} width={'920px'} height={'200px'} redraw={true} style={{'marginLeft':'2%'}}/>
+          </div>
+          <div style={{'marginTop': '3%', 'marginLeft':'7%', float:'left'}}>
+            <h3>Ethics Filter (by keywords)</h3>
+            <DoughnutChart data={doughnutDataEthics} options ={doughnutOptions} width={'200px'} height = {'200px'} redraw = {true} style={{'marginLeft':'20px','paddingTop':'20px'}}/>
           </div>
           <div style={{'marginTop': '3%', 'marginLeft':'5%', float:'left'}}>
-          <h3>Flags by Filters (this month)</h3>
-          <DoughnutChart data={doughnutData} options ={doughnutOptions} width={'150px'} height = {'150px'} redraw = {true} style={{'marginLeft':'20px'}}/>
+            <h3>Competitors Filter (by keywords)</h3>
+            <DoughnutChart data={doughnutDataCompetitors} options ={doughnutOptions} width={'200px'} height = {'200px'} redraw = {true} style={{'marginLeft':'20px','paddingTop':'20px'}}/>
           </div>
           <div style={{'marginTop': '3%', 'marginLeft':'5%', float:'left'}}>
-          <h3>Flags by Filters (this year)</h3>
-          <DoughnutChart data={doughnutData} options ={doughnutOptions} width={'150px'} height = {'150px'} redraw = {true} style={{'marginLeft':'20px'}}/>
+            <h3>Flags by Filters (this year)</h3>
+            <DoughnutChart data={doughnutDataEthics} options ={doughnutOptions} width={'200px'} height = {'200px'} redraw = {true} style={{'marginLeft':'20px','paddingTop':'20px'}}/>
           </div>
         </div>
+           <div style={{'marginTop':'8%','marginRight':'2%', float:'right'}}>
+                <h3>Most flagged users</h3>
+                <Table
+                  rowHeight={50}
+                  rowGetter={rowGetter}
+                  rowsCount={rows.length}
+                  width={300}
+                  height={800}
+                  headerHeight={30}>
+                  <Column
+                    label="user email"
+                    width={240}
+                    dataKey={0}
+                  />
+                  <Column
+                    label="# flagged"
+                    width={60}
+                    dataKey={1}
+                  />
+                </Table>
+          </div>
       </div>
       
     )
