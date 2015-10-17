c2b506b360beb2e7886da5d35b912812f770293f
Tito Esteves <titoesteves@live.com>
2015-10-17 16:59:38 -0700

    [feat](fix): ran jscs on all component files second time

--- src/components/FlaggedContext.jsx
+++ src/components/FlaggedContext.jsx
@@ -2,31 +2,30 @@ var React = require('react');
 
 var FlaggedContext = React.createClass({
 
-
-  render: function(){
-  var context = this.props.context;
-   return (
-    <div>
-      <table style={this.styles.table}>
-          <tr>
-            <th>Flagged Keyword</th>
-            <th>Context</th>
-          </tr>
-          <tr>
-            <td style={{color: '#ea6314', fontWeight:'bold'}}>{ this.props.flaggedKeyword }</td>
-            <td>{ this.props.callbacks._flagHighlightRender(this.props.context, [this.props.flaggedKeyword]) }</td>
-          </tr>
-      </table>
-    </div>
-    );
+  render: function() {
+    var context = this.props.context;
+    return (
+     <div>
+       <table style={this.styles.table}>
+           <tr>
+               <th>Flagged Keyword</th>
+               <th>Context</th>
+           </tr>
+           <tr>
+               <td style={{color: '#ea6314', fontWeight:'bold'}}>{ this.props.flaggedKeyword }</td>
+               <td>{ this.props.callbacks._flagHighlightRender(this.props.context, [this.props.flaggedKeyword]) }</td>
+           </tr>
+       </table>
+     </div>
+     );
   },
 
   styles:{
     table:{
       'marginTop':'0',
-      'tableLayout':'fixed'
-    }
-  }
+      'tableLayout':'fixed',
+    },
+  },
 });
 
 module.exports = FlaggedContext;
